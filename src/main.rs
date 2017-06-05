171b2a95d4c04c4c006ab35898fb7dcfc95c2234
Jorn van Wier <jornvanwier@gmail.com>
2017-06-05 12:05:46 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+extern crate chrono;
+extern crate clap;
+
+use chrono::prelude::*;
+use clap::{App, ArgMatches};
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufReader, BufWriter};
+use std::io::prelude::*;
+use std::fmt::Display;
+
+const DEFAULT_FILENAME: &str = "log.txt";
+
+fn main() {
+    let matches = get_arguments();
+
+    let file = get_file(&matches);
+    let mut file = BufWriter::new(file);
+
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+    let mut stdin = BufReader::new(stdin);
+
+    // Initialize a writer for stdout, only used when print is true
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
+    let mut stdout = BufWriter::new(stdout);
+
+    let print = matches.is_present("print");
+
+    let mut buf: String = String::new();
+
+    write(format!("Start log at {}\n", get_timestamp()),
+          &mut file,
+          &mut stdout,
+          print);
+    while stdin.read_line(&mut buf).unwrap() > 0 {
+        // Shouldn't use println here because the lines already contain a newline
+        let line = format!("[{}] {}", get_timestamp(), buf);
+
+        let _ = write(line, &mut file, &mut stdout, print);
+
+        buf.clear();
+    }
+}
+
+fn write<S: Display, WF: Write, WO: Write>(line: S,
+                                           file: &mut BufWriter<WF>,
+                                           stdout: &mut BufWriter<WO>,
+                                           print: bool) {
+    let _ = write!(file, "{}", line);
+
+    if print {
+        let _ = write!(stdout, "{}", line);
+    }
+}
+
+fn get_timestamp() -> String {
+    return Local::now().format("%F %T:0%.3f").to_string();
+}
+
+fn get_arguments() -> ArgMatches<'static> {
+    App::new("Tee Log")
+        .version("0.1")
+        .author("Jorn van Wier <jornvanwier@gmail.com>")
+        .about("Reads the output of a program and outputs it to a file with an added timestamp")
+        .args_from_usage("
+            [file] -f, --file <FILE> 'Sets the file to output to'
+            [print] -p, --print 'Print to stdout'
+        ")
+        .get_matches()
+}
+
+fn get_file(matches: &ArgMatches) -> File {
+    let file = matches.value_of("file").unwrap_or(DEFAULT_FILENAME);
+    OpenOptions::new()
+        .read(false)
+        .append(true)
+        .create(true)
+        .open(file)
+        .expect("Could not open log file")
+}
